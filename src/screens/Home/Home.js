c82f6056225d254c2637ef8c5269e72c5244b4ea
Rami Talisveiber <rami.timefreek@gmail.com>
2019-06-01 13:37:13 +0300

    animation

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -3,8 +3,43 @@ import './Home.css';
 import FlowerBox from '../../components/FlowerBox/FlowerBox.js'
 import logo from '../../assets/images/logoTransparent.png'
 import zerHahaveKfula from '../../assets/images/zerHahaveKfula.jpg'
+import callButton from '../../assets/images/callButton.png'
 
+import {animated, useSpring,config} from 'react-spring'
 
+function FirstContainer() {
+    const props = useSpring({marginTop: 0, from: {marginTop: 500 }})
+    // return <animated.div style={props}>I will fade in</animated.div>
+    return (
+        <animated.div style={props} className='firstContainer'>
+            <div className={'logoContainer'}>
+                <img src={logo} alt="Logo" className={'logoTransparent'}/>
+            </div>
+        </animated.div>
+    )
+}
+
+function ContentContainer(state) {
+    console.log("state",state)
+    const props = useSpring({marginTop: state.load ? 0 : 1000, from: {marginTop: 1000 }})
+    // return <animated.div style={props}>I will fade in</animated.div>
+    return (
+        <animated.div style={props}  className='contentContainer'>
+
+                {state.flowers.map(flower => {
+                    return (
+                        <FlowerBox
+                            key={Math.random() * flower.price}
+                            image={flower.image}
+                            name={flower.name}
+                            price={flower.price}
+                            story={flower.story}
+                        />
+                    )
+                })}
+        </animated.div>
+    )
+}
 class Home extends Component {
 
     constructor(props) {
@@ -30,31 +65,54 @@ class Home extends Component {
                     image: zerHahaveKfula,
                     story: ' איאקוליס וולופטה דיאם. וסטיבולום אט דולור, קראס אגת לקטוס וואל  איאקוליס וולופטה דיאם. איאקוליס וולופטה דיאם. וסטיבולום אט דולור, קראס אגת לקטוס וואל'
                 }
-            ]
+            ],
+            flowerLoad: false
+        }
+        this.myRef = React.createRef()
+    }
+
+
+    componentDidMount() {
+        this.handleScroll()
+    }
+
+
+    handleScroll = () => {
+        const {index, selected} = this.props
+        if (index === selected) {
+            const that = this
+            setTimeout(() => {
+                that.myRef.current.scrollIntoView({ behavior: 'smooth' })
+            }, 1000)
+            setTimeout(() => {
+                this.setState({flowerLoad:true})
+            }, 1300)
         }
     }
 
+
     render() {
+        console.log('this.state', this.state)
         const {flowers} = this.state
         return (
             <Fragment>
-                <div className='firstContainer'>
-                    <div className={'logoContainer'}>
-                        <img src={logo} alt="Logo" className={'logoTransparent'}/>
-                    </div>
-                </div>
-                <div className='contentContainer'>
-                    {flowers.map(flower => {
-                        return (
-                            <FlowerBox
-                                key={Math.random() * flower.price}
-                                image={flower.image}
-                                name={flower.name}
-                                price={flower.price}
-                                story={flower.story}
-                            />
-                        )
-                    })}
+                {/*<div className='firstContainer'>*/}
+                {/*<div className={'logoContainer'}>*/}
+                {/*<img src={logo} alt="Logo" className={'logoTransparent'}/>*/}
+                {/*<App/>*/}
+                {/*</div>*/}
+                {/*</div>*/}
+                <FirstContainer/>
+                <div ref={this.myRef}/>
+                <ContentContainer
+                    flowers={this.state.flowers}
+                load ={this.state.flowerLoad}
+                />
+                <div
+                    // className={'callButtonContainer'}
+                    style={callButtonContainer}
+                >
+                    <img src={callButton} alt="Logo" className={'callButtonContainer'}/>
                 </div>
 
             </Fragment>
@@ -63,3 +121,13 @@ class Home extends Component {
 }
 
 export default Home;
+
+const callButtonContainer = {
+    width: 15,
+height: 15,
+marginLeft: 5,
+position: 'fixed',
+left: 0,
+bottom: 2,
+zIndex: 3,
+}
