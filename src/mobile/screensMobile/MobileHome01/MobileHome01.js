e9ddb7d22f57e839507e31b0d7711b4032330316
Rami Talisveiber <rami.timefreek@gmail.com>
2019-07-02 22:22:55 +0300

    scrollSafariDisableOnSwipe

--- src/mobile/screensMobile/MobileHome01/MobileHome01.js
+++ src/mobile/screensMobile/MobileHome01/MobileHome01.js
@@ -19,6 +19,35 @@ import Linkedin from 'react-icons/lib/fa/linkedin';
 
 
 class MobileHome01 extends Component {
+    componentDidMount(){
+        window.addEventListener('touchstart', this.touchStart);
+        window.addEventListener('touchmove', this.preventTouch, {passive: false});
+    }
+
+    componentWillUnmount(){
+        window.removeEventListener('touchstart', this.touchStart);
+        window.removeEventListener('touchmove', this.preventTouch, {passive: false});
+    }
+
+    touchStart(e){
+        this.firstClientX = e.touches[0].clientX;
+        this.firstClientY = e.touches[0].clientY;
+    }
+
+    preventTouch(e){
+        const minValue = 5; // threshold
+
+        this.clientX = e.touches[0].clientX - this.firstClientX;
+        this.clientY = e.touches[0].clientY - this.firstClientY;
+
+        // Vertical scrolling does not work when you start swiping horizontally.
+        if(Math.abs(this.clientX) > minValue){
+            e.preventDefault();
+            e.returnValue = false;
+            return false;
+        }
+    }
+
     render() {
         const settings = {
             dots: true,
