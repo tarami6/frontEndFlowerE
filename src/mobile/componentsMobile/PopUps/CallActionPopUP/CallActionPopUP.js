04f0c4e01d2f5aface217fc25ba7cdf9632f8f20
Rami Talisveiber <rami.timefreek@gmail.com>
2019-08-31 15:33:24 +0300

    ver:1.1.5

--- src/mobile/componentsMobile/PopUps/CallActionPopUP/CallActionPopUP.js
+++ src/mobile/componentsMobile/PopUps/CallActionPopUP/CallActionPopUP.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Zoom from 'react-reveal/Fade';
+import Phone from 'react-icons/lib/fa/phone';
+import WhatsApp from 'react-icons/lib/fa/whatsapp';
 
 import Close from "react-icons/lib/md/clear";
 
@@ -18,11 +20,11 @@ const CallActionPopUP = (props) => {
                     </div>
                     <div className={'buttonsHolder'}>
                         <div className={'callToButton'} onClick={() => props.eventGA("Phone", "Phone pressed", props.page)}>
-                            <a lassName={'callToButton'} href="tel:0546712717"  >חייג </a>
+                            <a className={'callToButton'} href="tel:0546712717"  > חייג  <Phone size={20} color={"#efc368"} style={{marginRight: 5}} /> </a>
                         </div>
                         <div className={'separatorCallButton'}/>
                         <div className={'whatUpButton'} onClick={() => props.eventGA("WhatsUpButton", "WhatsUpButton button pressed", props.page)}>
-                            <a  href="https://wa.me/+972546712717/?text=שלום אני רוצה להזמין זר" >WhatsApp</a>
+                            <a  href="https://wa.me/+972546712717/?text=שלום אני רוצה להזמין זר" ><WhatsApp size={20} color={"darkgreen"} style={{marginLeft: 5}} /> WhatsApp </a>
                         </div>
                     </div>
                 </div>
