72b10463a69a901d17f8584c769be5e3cc5ea049
Rami Talisveiber <rami.timefreek@gmail.com>
2019-09-16 16:26:05 +0300

    removeChanges

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 import React, {Component} from 'react';
 import {Route, Router} from 'react-router-dom';
 import {isMobile} from 'react-device-detect';
-import asyncComponent from '../src/AsyncComponent'
+import MobileHome01 from './mobile/screensMobile/MobileHome01/MobileHome01';
+import ProductPage from './mobile/screensMobile/ProductPage/ProductPage';
+import DeskHome1 from './desktop/screensDesktop/DeskHome1/DeskHome1';
+import DeskProductPage from './desktop/screensDesktop/DeskProductPage/DeskProductPage';
+import Pot from './mobile/screensMobile/Pot/Pot'
 import ReactGA from 'react-ga';
 import ReactPixel from 'react-facebook-pixel';
 import TagManager from 'react-gtm-module'
@@ -9,22 +13,6 @@ import smoothscroll from 'smoothscroll-polyfill';
 import {createBrowserHistory} from "history";
 import { hotjar } from 'react-hotjar';
 import LiveChat from 'react-livechat'
-
-const MobileHome01 = asyncComponent(() =>
-    import('./mobile/screensMobile/MobileHome01/MobileHome01').then(module => module.default)
-)
-const ProductPage = asyncComponent(() =>
-    import('./mobile/screensMobile/ProductPage/ProductPage').then(module => module.default)
-)
-const DeskHome1 = asyncComponent(() =>
-    import('./desktop/screensDesktop/DeskHome1/DeskHome1').then(module => module.default)
-)
-const DeskProductPage = asyncComponent(() =>
-    import('./desktop/screensDesktop/DeskProductPage/DeskProductPage').then(module => module.default)
-)
-const Pot = asyncComponent(() =>
-    import('./mobile/screensMobile/Pot/Pot').then(module => module.default)
-)
 
 const tagManagerArgs = {
     gtmId: 'GTM-T5TRHP2'
